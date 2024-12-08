e0a5b46ee89ef1f35d81fb75ead74d8c73116357
Zalak Nrupay Shah <oel232ec@students.codeinstitute.net>
2024-12-08 23:21:45 +0000

    multiply code functioning

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,62 +1,73 @@
-document.addEventListener('DOMContentLoaded', function() {
-   let buttons = document.getElementsByTagName("button");
-   for (let button of buttons){
-        button.addEventListener('click', function(){
-             
-             if (this.getAttribute("data-type") == "submit"){
+document.addEventListener('DOMContentLoaded', function () {
+    let buttons = document.getElementsByTagName("button");
+    for (let button of buttons) {
+        button.addEventListener("click", function () {
+
+            if (this.getAttribute("data-type") == "submit") {
                 checkAnswer();
-             } else{
+            } else {
                 let gameType = this.getAttribute("data-type");
                 runGame(gameType);
-             }
-        })
+            }
+        });
     }
-   runGame("addition");
-})
- function runGame(gameType){
+    runGame("addition");
+});
+function runGame(gameType) {
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
-    if (gameType === "addition"){
+    if (gameType === "addition") {
         displayAdditionQuestion(num1, num2);
+    } else if (gameType === "multiply") {
+        displayMultiplyQuestion(num1, num2);
+
     } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
     }
- }
- function displayAdditionQuestion(operand1, operand2){
+}
+function displayAdditionQuestion(operand1, operand2) {
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "+";
- }
- function calculateCorrectAnswer() {
-    let operand1 = parseInt(document.getElementById('operand1').innerText);
-    let operand2 = parseInt(document.getElementById('operand2').innerText);
-    let operator = document.getElementById('operator').textContent;
-    if (operator === "+"){
-        return [operand1 + operand2, "addition"];
-    } else {
-        alert(`Unimplemented operator ${operator}`);
-        throw `Unimplemented operator ${operator}. Aborting!`;
+}
+function displayMultiplyQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "x";
+}
+    function calculateCorrectAnswer() {
+        let operand1 = parseInt(document.getElementById('operand1').innerText);
+        let operand2 = parseInt(document.getElementById('operand2').innerText);
+        let operator = document.getElementById('operator').textContent;
+        if (operator === "+") {
+            return [operand1 + operand2, "addition"];
+        } else if (operator === "x") {
+            return [operand1 * operand2, "multiply"];
+
+        } else {
+            alert(`Unimplemented operator ${operator}`);
+            throw `Unimplemented operator ${operator}. Aborting!`;
+        }
     }
- }
- function checkAnswer(){
-    let userAnswer = parseInt(document.getElementById('answer-box').value);
-    let calculatedAnswer = calculateCorrectAnswer();
-    let isCorrect = userAnswer === calculatedAnswer[0];
-    if (isCorrect){
-        alert("Hey! You got it right! :D");
-        incrementScore();
-    } else {
-        alert(`Awwww....you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
-        incrementWrongAnswer();
+    function checkAnswer() {
+        let userAnswer = parseInt(document.getElementById('answer-box').value);
+        let calculatedAnswer = calculateCorrectAnswer();
+        let isCorrect = userAnswer === calculatedAnswer[0];
+        if (isCorrect) {
+            alert("Hey! You got it right! :D");
+            incrementScore();
+        } else {
+            alert(`Awwww....you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+            incrementWrongAnswer();
+        }
+        runGame(calculatedAnswer[1]);
     }
-    runGame(calculatedAnswer[1]);
- }
-  function incrementScore(){
-    let oldScore = parseInt(document.getElementById('score').innerText);
-    document.getElementById('score').innerText = ++oldScore;
-  }
-  function incrementWrongAnswer(){
-    let oldScore = parseInt(document.getElementById('incorrect').innerText);
-    document.getElementById('incorrect').innerText = ++oldScore;
-  }
+    function incrementScore() {
+        let oldScore = parseInt(document.getElementById('score').innerText);
+        document.getElementById('score').innerText = ++oldScore;
+    }
+    function incrementWrongAnswer() {
+        let oldScore = parseInt(document.getElementById('incorrect').innerText);
+        document.getElementById('incorrect').innerText = ++oldScore;
+    }
