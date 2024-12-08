a13c9972c9d6e1db3a2cc1d7fdca1b6e8a0a2603
Zalak Nrupay Shah <oel232ec@students.codeinstitute.net>
2024-12-08 22:28:48 +0000

    checking right answer or not working

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', function() {
         button.addEventListener('click', function(){
              
              if (this.getAttribute("data-type") == "submit"){
-                alert("You clicked Submit");
+                checkAnswer();
              } else{
                 let gameType = this.getAttribute("data-type");
                 runGame(gameType);
@@ -27,4 +27,26 @@ document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "+";
+ }
+ function calculateCorrectAnswer() {
+    let operand1 = parseInt(document.getElementById('operand1').innerText);
+    let operand2 = parseInt(document.getElementById('operand2').innerText);
+    let operator = document.getElementById('operator').textContent;
+    if (operator === "+"){
+        return [operand1 + operand2, "addition"];
+    } else {
+        alert(`Unimplemented operator ${operator}`);
+        throw `Unimplemented operator ${operator}. Aborting!`;
+    }
+ }
+ function checkAnswer(){
+    let userAnswer = parseInt(document.getElementById('answer-box').value);
+    let calculatedAnswer = calculateCorrectAnswer();
+    let isCorrect = userAnswer === calculatedAnswer[0];
+    if (isCorrect){
+        alert("Hey! You got it right! :D");
+    } else {
+        alert(`Awwww....you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+    }
+    runGame(calculatedAnswer[1]);
  }
