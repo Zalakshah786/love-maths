b9bea705530fc9bb1ce5e206a4358dcdd55233ef
Zalak Nrupay Shah <oel232ec@students.codeinstitute.net>
2024-12-08 22:54:48 +0000

    counting right and wrong answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -45,8 +45,18 @@ document.addEventListener('DOMContentLoaded', function() {
     let isCorrect = userAnswer === calculatedAnswer[0];
     if (isCorrect){
         alert("Hey! You got it right! :D");
+        incrementScore();
     } else {
         alert(`Awwww....you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+        incrementWrongAnswer();
     }
     runGame(calculatedAnswer[1]);
  }
+  function incrementScore(){
+    let oldScore = parseInt(document.getElementById('score').innerText);
+    document.getElementById('score').innerText = ++oldScore;
+  }
+  function incrementWrongAnswer(){
+    let oldScore = parseInt(document.getElementById('incorrect').innerText);
+    document.getElementById('incorrect').innerText = ++oldScore;
+  }
